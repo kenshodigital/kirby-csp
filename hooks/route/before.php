5455e8efd6d0ccfad8ec1341417d58e74ecbf9bc
Jens Scherbl <jens@scherbl.com>
2025-12-03 13:56:21 +0100

    Simplifies implementation

--- hooks/route/before.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-return function (): void {
-	$this->response()->header(
-		'Content-Security-Policy',
-		"script-src 'nonce-{$this->nonce()}' 'strict-dynamic' 'unsafe-inline' https:; object-src 'none'; base-uri 'none';",
-	);
-};
--- index.php
+++ index.php
@@ -2,8 +2,16 @@
 
 use Kirby\Cms\App;
 
-App::plugin('kensho/csp', [
+App::plugin(name: 'kensho/csp', extends: [
 	'hooks' => [
-		'route:before' => require __DIR__ . '/hooks/route/before.php',
+		'route:before' => function (): void {
+			/**
+			 * @var App $this
+			 */
+			$this->response()->header(
+				key: 'Content-Security-Policy',
+				value: "script-src 'nonce-{$this->nonce()}' 'strict-dynamic' 'unsafe-inline' https:; object-src 'none'; base-uri 'none';",
+			);
+		},
 	],
 ]);
