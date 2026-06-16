481f083566776ee0683f0a382de0c86e79a15a56
Jens Scherbl <jens@scherbl.com>
2026-06-16 17:46:05 +0200

    Restricts CSP header to HTML pages

--- index.php
+++ index.php
@@ -6,14 +6,17 @@ use Kirby\Cms\App;
 
 App::plugin(name: 'kensho/csp', extends: [
 	'hooks' => [
-		'route:before' => function (): void {
-			/**
-			 * @var App $this
-			 */
-			$this->response()->header(
-				key: 'Content-Security-Policy',
-				value: "script-src 'nonce-{$this->nonce()}' 'strict-dynamic'; object-src 'none'; base-uri 'none';",
-			);
+		'page.render:before' => function (string $contentType, array $data): array {
+			if ($contentType === 'html') {
+				/**
+				 * @var App $this
+				 */
+				$this->response()->header(
+					key: 'Content-Security-Policy',
+					value: "script-src 'nonce-{$this->nonce()}' 'strict-dynamic'; object-src 'none'; base-uri 'none';",
+				);
+			}
+			return $data;
 		},
 	],
 ]);
